3bc1389bd5b93adbe1ad0ffccd4b6eb439cee007
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-07-06 15:21:28 +0100

    Added the ability to allow user to set a password when creating new user

--- src/handlers/newUser.ts
+++ src/handlers/newUser.ts
@@ -2,20 +2,31 @@ import { NextFunction, Request, Response } from "express";
 import { createUser } from "../db/queries/users.js";
 import { NewUser } from "../db/schema.js";
 import { BadRequestError } from "../middleware/errorHandler.js";
+import { hashPassword } from "src/auth/auth.js";
 
 export async function handlerNewUser(req: Request, res: Response, next: NextFunction) {
     type emailReq = {
         email: string
     }
 
-    if("email" in req.body){
-        if (typeof req.body.email === "string"){
+    if(req.body.email && req.body.password){
+        if (typeof req.body.email === "string" && typeof req.body.password == "string"){
             const user: NewUser = {
-                email: req.body.email
+                email: req.body.email,
+                hashedPassword: await hashPassword(req.body.password)
             };
 
             const createdUser = await createUser(user);
-            res.status(201).json(createdUser);
+
+            type UserInfo = Omit<NewUser, "hashedPassword">
+            const userInfo: UserInfo = {
+                id: createdUser.id,
+                createdAt: createdUser.createdAt,
+                updatedAt: createdUser.updatedAt,
+                email: createdUser.email,
+            }
+
+            res.status(201).json(userInfo);
         } else {
             throw new BadRequestError("Incorrect email format");
         }
